1166833c276d0d280066e44fb0db977057c6a5ef
Nikhil <lanjewarnikhil90@gmail.com>
2024-02-07 15:45:28 +0530

    Crop and Product Entity Added
    Crop and Product Entity Added

---
+++ Backend/src/main/java/com/app/entities/Product.java
@@ -0,0 +1,27 @@
+package com.app.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Setter
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product extends BaseEntity {
+	@Column(name = "Rate",nullable = false)
+	private Double rate;
+	
+	@Column(name = "Product_Name",length = 20,nullable = false)
+	private String product_nameString;
+	
+	
+}
