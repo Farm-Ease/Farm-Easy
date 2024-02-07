3a7e38838fdac08d42b1eb325bc081c2c5af58a5
Abhii <yadavabhinav1010@gmail.com>
2024-02-07 15:51:35 +0530

    ApmcAppointment.java, Counsellor.java

---
+++ Backend/src/main/java/com/app/entities/Counsellor.java
@@ -0,0 +1,27 @@
+package com.app.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Counsellor extends BaseEntity {
+
+	@Column(length = 30)
+	private String firstName;
+	@Column(length = 30)
+	private String lastName;
+	@Column(length = 30, unique = true) // =>unique
+	private String email;
+	@Column(nullable = false)
+	private long mobileNo;
+	private String state;
+	private String district;
+	private String village;
+}
