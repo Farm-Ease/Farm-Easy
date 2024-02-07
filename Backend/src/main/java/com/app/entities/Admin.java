f9dfecc0b2793ce394aeb0ee3662e61db6ef9c69
Your hiwarkarved7@gmail.com <you@example.com>
2024-02-07 15:59:54 +0530

    farmer entity added

---
+++ Backend/src/main/java/com/app/entities/Admin.java
@@ -0,0 +1,26 @@
+package com.app.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name = "admin")
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Admin extends BaseEntity{
+	@Column(length = 30)
+	private String firstName;
+	@Column(length = 30)
+	private String lastName;
+	@Column(length = 30, unique = true)
+	private String email;
+	@Column(nullable = false)
+	private String password;
+}
