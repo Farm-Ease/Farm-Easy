019b5f98099001e6a0d867be49689ca73e986d38
Vedant Jitendra Hiwarkar <72973893+hiwarkarved@users.noreply.github.com>
2024-02-08 12:10:55 +0530

    Farmer Controller Service and DTOS created

--- Backend/src/main/java/com/app/dto/FarmerDTO.java
+++ Backend/src/main/java/com/app/dto/FarmerDTO.java
@@ -1,6 +1,11 @@
 package com.app.dto;
 
+import java.time.LocalDate;
+
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
@@ -18,9 +23,22 @@ public class FarmerDTO {
 	@NotBlank
 	private String name;
 	@NotBlank
+	private String email;
+	@JsonProperty(access = Access.WRITE_ONLY) //required only in de-ser.
+	private String password;
+	@JsonProperty(access = Access.WRITE_ONLY)
+	private String confirmPassword;
+	@NotBlank
+	private String adhaar;
+	@NotBlank
 	private String village;
 	@NotBlank
 	private String district;
 	@NotBlank
 	private String state;
+	@NotBlank
+	private String number;
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	private LocalDate dob;
+	
 }
--- Backend/src/main/java/com/app/service/FarmerServiceImpl.java
+++ Backend/src/main/java/com/app/service/FarmerServiceImpl.java
@@ -2,6 +2,8 @@ package com.app.service;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,7 @@ import com.app.dto.ApiResponse;
 import com.app.dto.ApmcAppointmentDTO;
 import com.app.dto.CounsellorDTO;
 import com.app.dto.FarmerAppointmentDTO;
+import com.app.dto.FarmerDTO;
 import com.app.entities.ApmcAppointment;
 import com.app.entities.Counsellor;
 import com.app.entities.Farmer;
@@ -51,6 +54,40 @@ public class FarmerServiceImpl implements FarmerService{
 		appointmentDao.delete(appt);
 		return new ApiResponse("Appointment with id"+appt.getId()+"deleted....");
 	}
+
+
+
+
+
+	@Override
+	public ApmcAppointmentDTO updateAppoitnment(Long appointmentId, @Valid ApmcAppointmentDTO apptDto) {
+		ApmcAppointment appt = appointmentDao.findById(appointmentId).orElseThrow(()->new ResourceNotFoundException("Invalid appt id"));
+		appt.setDate(apptDto.getDate());
+		appt.setQuantity(apptDto.getQuantity());
+		//appt.setProducts(apptDto.getProducts());
+		appt.setTransaction(apptDto.getTransaction());
+		return mapper.map(appt, ApmcAppointmentDTO.class);
+	}
+
+
+
+
+
+	@Override
+	public FarmerDTO updateFarmer(Long farmerId, @Valid FarmerDTO farmerDto) {
+		// TODO Auto-generated method stub
+		Farmer  farmer = farmerDao.findById(farmerId).orElseThrow(()-> new ResourceNotFoundException("Invalid farmer id"));
+		farmer.setName(farmerDto.getName());
+		farmer.setNumber(farmerDto.getNumber());
+		farmer.setAdhaar(farmerDto.getAdhaar());
+		farmer.setState(farmerDto.getState());
+		farmer.setDistrict(farmerDto.getDistrict());
+		farmer.setVillage(farmerDto.getVillage());
+		
+		return mapper.map(farmer, FarmerDTO.class);
+	}
+	
+	
 	
 	
 	
